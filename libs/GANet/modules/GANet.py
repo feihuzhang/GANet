0e33159fcc2a8c608bf32dccacc44f823638c5fc
feihuzhang <hi.yexu@gmail.com>
2019-07-28 18:58:07 +0100

    Update GANet.py

--- libs/GANet/modules/GANet.py
+++ libs/GANet/modules/GANet.py
@@ -115,13 +115,13 @@ class LGA(Module):
 class GetCostVolume(Module):
     def __init__(self, maxdisp):
         super(GetCostVolume, self).__init__()
-        self.maxdisp=maxdisp+1
+        self.maxdisp = maxdisp + 1
 
-    def forward(self, x,y):
+    def forward(self, x, y):
         assert(x.is_contiguous() == True)
         with torch.cuda.device_of(x):
             num, channels, height, width = x.size()
-            cost = x.new().resize_(num, channels*2, self.maxdisp, height, width).zero_()
+            cost = x.new().resize_(num, channels * 2, self.maxdisp, height, width).zero_()
 #            cost = Variable(torch.FloatTensor(x.size()[0], x.size()[1]*2, self.maxdisp,  x.size()[2],  x.size()[3]).zero_(), volatile= not self.training).cuda()
             for i in range(self.maxdisp):
                 if i > 0 :
@@ -136,15 +136,15 @@ class GetCostVolume(Module):
  
 class DisparityRegression(Module):
     def __init__(self, maxdisp):
-        self.maxdisp=maxdisp+1
         super(DisparityRegression, self).__init__()
+	self.maxdisp = maxdisp + 1
 #        self.disp = Variable(torch.Tensor(np.reshape(np.array(range(self.maxdisp)),[1,self.maxdisp,1,1])).cuda(), requires_grad=False)
 
     def forward(self, x):
         assert(x.is_contiguous() == True)
         with torch.cuda.device_of(x):
-            disp = Variable(torch.Tensor(np.reshape(np.array(range(self.maxdisp)),[1,self.maxdisp,1,1])).cuda(), requires_grad=False)
-            disp = disp.repeat(x.size()[0],1,x.size()[2],x.size()[3])
-            out = torch.sum(x*disp,1)
+            disp = Variable(torch.Tensor(np.reshape(np.array(range(self.maxdisp)),[1, self.maxdisp, 1, 1])).cuda(), requires_grad=False)
+            disp = disp.repeat(x.size()[0], 1, x.size()[2], x.size()[3])
+            out = torch.sum(x * disp, 1)
         return out
 
