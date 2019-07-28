240db34b0f65ce71d61cbb6de1ad205e756678f9
feihuzhang <hi.yexu@gmail.com>
2019-07-28 17:00:09 +0100

    Update predict.sh

--- predict.sh
+++ predict.sh
@@ -1,6 +1,7 @@
 
 python predict.py --crop_height=384 \
                   --crop_width=1248 \
+                  --max_disp=192 \
                   --data_path='/ssd1/zhangfeihu/data/kitti/2015//testing/' \
                   --test_list='lists/kitti2015_test.list' \
                   --save_path='./result/' \
@@ -10,6 +11,7 @@ exit
 
 python predict.py --crop_height=384 \
                   --crop_width=1248 \
+                  --max_disp=192 \
                   --data_path='/media/feihu/Storage/stereo/kitti/testing/' \
                   --test_list='lists/kitti2012_test.list' \
                   --save_path='./result/' \
